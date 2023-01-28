2bcff5563cd2882affe24abf551238b0b4df50e4
Mongark <mongarkprogrammer@gmail.com>
2023-01-28 10:01:50 -0400

    Fixed SideNav

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import {Dashboard} from '@material-ui/icons'
 import {
   Route,
   Routes,
 } from 'react-router-dom'
 
 import './App.css'
+import Books from './Pages/Books/Books'
+import Dashboard from './Pages/Dashboard/Dashboard'
+import SideNav from './Components/SideNav/SideNav'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />}/>
-    </Routes>
+    <div className='App'>
+      <SideNav />
+
+      <Routes>
+        <Route path="/" element={<Dashboard />}/>
+        <Route path="/books" element={<Books />}/>
+      </Routes>
+    </div>
   )
 }
 
